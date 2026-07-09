01091a7691fd304dc23cc52b1ae27ef94ddb7c70
mistrmochov <matejdvoracek@fastmail.com>
2026-07-09 02:50:01 +0200

    fix(Scripts/build_kernel.sh) make sure version file exists

--- Scripts/build_kernel.sh
+++ Scripts/build_kernel.sh
@@ -78,6 +78,9 @@ if [ "${BBG_SUPPORT}" = "true" ]; then
 fi
 
 cat "${EXTRA_CFG}" >> out/.config
+if [ -f version ]; then
+  cp version out/version
+fi
 make ${MAKE_ARGS} olddefconfig
 [ -f scripts/setlocalversion ] && sed -i 's/-dirty//g' scripts/setlocalversion || true
 
